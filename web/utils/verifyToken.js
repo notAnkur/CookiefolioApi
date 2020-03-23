77ed6b9ea0048b8d50518001d5852e282322a884
Ankur Anant <ankuranant99@gmail.com>
2020-03-23 11:25:24 +0530

    catch token verification error

--- web/utils/verifyToken.js
+++ web/utils/verifyToken.js
@@ -1,10 +1,14 @@
 const verifyToken = (req, res, next) => {
 	const bearerHeader = req.headers["authorization"];
 	if (typeof bearerHeader !== "undefined") {
-		const bearer = bearerHeader.replace(/^JWT\s/, ``).split(" ");
-		const bearerToken = bearer[1].replace(/^"(.*)"$/, "$1");
-		req.token = bearerToken;
-		next();
+		try {
+			const bearer = bearerHeader.replace(/^JWT\s/, ``).split(" ");
+			const bearerToken = bearer[1].replace(/^"(.*)"$/, "$1");
+			req.token = bearerToken;
+			next();
+		} catch(error) {
+			res.status(400).json({isOpSuccess: false, message: "Check token format"});
+		}
 	} else {
 		res.status(400).json({isOpSuccess: false, message: "Invalid token"});
 	}
